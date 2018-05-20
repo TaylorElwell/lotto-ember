2488a0aaa9fe1ce8c5da80613cbbd1973c04897b
TaylorElwell <telwel1@lsu.edu>
2018-05-20 11:25:08 -0500

    cleaning

--- app/routes/scientists.js
+++ app/routes/scientists.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    return ['Suh Dude', 'jhwrb wekjfb', 'jvsf jshf'];
+    return ['Suh Dude', 'Spongebob', 'Jeff'];
   }
 });
